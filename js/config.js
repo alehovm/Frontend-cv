ca931f05f308234884d673932ca6c24726d0ef95
Sidney <sidneyca@live.com>
2024-02-15 13:50:01 +0100

    new config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-north-1_Gc3S9d1mh', // e.g. eu-east-2_uXboG5pAb
+        userPoolClientId: '7rd5bu8lbef3sovmm6ejegnk39', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
