5f870410ddc6b697287bad3f682d001e770ac5c9
EC2 Default User <ec2-user@ip-172-31-13-170.eu-north-1.compute.internal>
2024-02-20 12:47:20 +0000

    method change

--- getSignedURL/lambda_function.py
+++ getSignedURL/lambda_function.py
@@ -15,27 +15,36 @@ def get_upload_url(event):
     random_id = str(random.randint(1, 10000000))
     key = f"{random_id}-video.mp4"
 
-    # Generate a presigned URL S3 POST request to upload a file with a specific key
-    upload_url = s3.generate_presigned_post(Bucket=os.environ['UploadBucket'],
-                                            Key=key,
-                                            Fields={'acl': 'public-read'},
-                                            Conditions=[{'acl': 'public-read'}],
-                                            ExpiresIn=URL_EXPIRATION_SECONDS)
+    # Get signed URL from S3
+    try:
+        upload_url = s3.generate_presigned_post(Bucket=os.environ['UploadBucket'],
+                                                Key=key,
+                                                Fields={'acl': 'public-read'},
+                                                Conditions=[{'acl': 'public-read'}],
+                                                ExpiresIn=URL_EXPIRATION_SECONDS)
+    except s3.exceptions.S3Error as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error generating presigned URL')
+        }
+
+    objectURL = f"https://{os.environ['UploadBucket']}.s3.amazonaws.com/{key}"
     
-    # Construct the response object
-    response = {
+    # Generate the response object
+    responseBody = {
+        'uploadURL': {
+            'url': upload_url['url'],
+            'fields': upload_url['fields']
+        },
+        'Key': key,
+        'objectURL': objectURL
+    }
+
+    return {
         "statusCode": 200,
         "headers": {
             "content-type": "application/json; charset=utf-8"
         },
-        "body": json.dumps({
-            'uploadURL': {
-                'url': upload_url['url'],  # URL to send the POST request to
-                'fields': upload_url['fields']  # Fields to include in the POST request
-            },
-            'Key': key,  # The key that should be used to access the file after upload
-            'objectURL': f"https://{os.environ['UploadBucket']}.s3.eu-north-1.amazonaws.com/{key}"  # Public URL to access the uploaded file
-        })
+        "body": json.dumps(responseBody)
     }
-
-    return response
