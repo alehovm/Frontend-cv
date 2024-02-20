2e8616b5aa630b922464e6bea36f0072774339ab
EC2 Default User <ec2-user@ip-172-31-13-170.eu-north-1.compute.internal>
2024-02-20 12:40:27 +0000

    method change

--- getSignedURL/lambda_function.py
+++ getSignedURL/lambda_function.py
@@ -15,25 +15,27 @@ def get_upload_url(event):
     random_id = str(random.randint(1, 10000000))
     key = f"{random_id}-video.mp4"
 
-    # Get signed URL from S3
+    # Generate a presigned URL S3 POST request to upload a file with a specific key
     upload_url = s3.generate_presigned_post(Bucket=os.environ['UploadBucket'],
                                             Key=key,
                                             Fields={'acl': 'public-read'},
                                             Conditions=[{'acl': 'public-read'}],
-                                            ExpiresIn=3600)
-    objectURL = f"https://{os.environ['UploadBucket']}.s3.eu-north-1.amazonaws.com/{key}"
+                                            ExpiresIn=URL_EXPIRATION_SECONDS)
     
-    responseBody = json.dumps({
-        'uploadURL': upload_url,
-        'Key': key,
-        'objectURL': objectURL
-        })
+    # Construct the response object
     response = {
-            "statusCode": 200,
-            "headers": {
-                "content-type": "application/json; charset=utf-8"
+        "statusCode": 200,
+        "headers": {
+            "content-type": "application/json; charset=utf-8"
+        },
+        "body": json.dumps({
+            'uploadURL': {
+                'url': upload_url['url'],  # URL to send the POST request to
+                'fields': upload_url['fields']  # Fields to include in the POST request
             },
-            "body": responseBody
-        }
+            'Key': key,  # The key that should be used to access the file after upload
+            'objectURL': f"https://{os.environ['UploadBucket']}.s3.eu-north-1.amazonaws.com/{key}"  # Public URL to access the uploaded file
+        })
+    }
 
     return response
