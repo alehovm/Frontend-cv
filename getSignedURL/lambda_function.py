86d9764abdc5e9a443604cf580cba8df5111ab4a
EC2 Default User <ec2-user@ip-172-31-13-170.eu-north-1.compute.internal>
2024-02-21 07:57:43 +0000

    new axios

--- getSignedURL/lambda_function.py
+++ getSignedURL/lambda_function.py
@@ -1,4 +1,3 @@
-# Backend (Lambda function)
 import boto3
 import json
 import random
@@ -7,6 +6,7 @@ import os
 # Change this value to adjust the signed URL's expiration
 URL_EXPIRATION_SECONDS = 300
 
+# Main Lambda entry point
 def lambda_handler(event, context):
     return get_upload_url(event)
 
@@ -15,36 +15,25 @@ def get_upload_url(event):
     random_id = str(random.randint(1, 10000000))
     key = f"{random_id}-video.mp4"
 
-    try:
-        upload_url = s3.generate_presigned_post(Bucket=os.environ['UploadBucket'],
-                                                Key=key,
-                                                Fields={'acl': 'public-read'},
-                                                Conditions=[{'acl': 'public-read'}],
-                                                ExpiresIn=URL_EXPIRATION_SECONDS)
-    except s3.exceptions.S3Error as e:
-        print(e)
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Error generating presigned URL')
-        }
-
-    objectURL = f"https://{os.environ['UploadBucket']}.s3.amazonaws.com/{key}"
+    # Get signed URL from S3
+    upload_url = s3.generate_presigned_post(Bucket=os.environ['UploadBucket'],
+                                            Key=key,
+                                            Fields={'acl': 'public-read'},
+                                            Conditions=[{'acl': 'public-read'}],
+                                            ExpiresIn=3600)
+    objectURL = f"https://{os.environ['UploadBucket']}.s3.eu-north-1.amazonaws.com/{key}"
     
-    # Note that responseBody is a dict, not a string
-    responseBody = {
-        'uploadURL': {
-            'url': upload_url['url'],
-            'fields': upload_url['fields']
-        },
+    responseBody = json.dumps({
+        'uploadURL': upload_url,
         'Key': key,
         'objectURL': objectURL
-    }
+        })
+    response = {
+            "statusCode": 200,
+            "headers": {
+                "content-type": "application/json; charset=utf-8"
+            },
+            "body": responseBody
+        }
 
-    # The entire responseBody is JSON stringified, not just the upload_url
-    return {
-        "statusCode": 200,
-        "headers": {
-            "content-type": "application/json; charset=utf-8"
-        },
-        "body": json.dumps(responseBody)
-    }
+    return response
